3fa23225cff193b31c620ec5a850ccc777f551aa
kaydeeloka <89924482+kaydeeloka@users.noreply.github.com>
2023-06-14 21:08:31 +0900

    abtevent.js

--- js/abtevent.js
+++ js/abtevent.js
@@ -1,48 +1,44 @@
+//declare variables for container box and image 
 var fullImgBox = document.getElementById("fullImgBox");
 var fullImg = document.getElementById("fullImg");
+//array that link to the image to be display
 var images = ["img/event_1.jpg", "img/event_2.jpg", "img/event_3.jpg", "img/event_4.jpg", "img/event_1.jpg", "img/event_6.jpg", "img/event_7.jpg", "img/cls_2.jpg", "img/event_8.jpg"];
 var currentIndex = 0;
 
+//function when image is clicked, 
+//it will display the image full size
 function openfullImg(pic){
     fullImgBox.style.display = "flex";
     fullImg.src = pic;
 }
 
+//function to close the full-img box
 function closefullImg(){
     fullImgBox.style.display = "none";
     fullImg.src ="";    
 }
 
+//function to show previous image when the previous arrow is clicked
 function showPrevImg() {
     currentIndex--;
     if (currentIndex < 0) {
       currentIndex = images.length - 1;
     }
+      //update the image
     fullImg.src = images[currentIndex];
-    updateArrows();
+
   }
   
+  //function to show next image when the next arrow is clicked
   function showNextImg() {
     currentIndex++;
     if (currentIndex >= images.length) {
       currentIndex = 0;
     }
+    //update the image
     fullImg.src = images[currentIndex];
-    updateArrows();
   }
   
-  function updateArrows() {
-    var prevArrow = document.getElementById("prev");
-    var nextArrow = document.getElementById("next");
-    
-    if (images.length <= 1) {
-      prevArrow.style.display = "none";
-      nextArrow.style.display = "none";
-    } else {
-      prevArrow.style.display = "block";
-      nextArrow.style.display = "block";
-    }
-  }
-  
+  //call the function to move the image after clicked
   document.getElementById("prev").addEventListener("click", showPrevImg);
   document.getElementById("next").addEventListener("click", showNextImg);
